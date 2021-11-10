aa8e1587385843c9db1377e7a263bd854b241de1
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-11-10 16:04:53 +0100

    più carino

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var miovue = new Vue({
     el: "#container",
     data: {
-        saluto: "hello world",
+        saluto: "Hello World",
         immagine: "https://i1.wp.com/www.giacomocusano.com/wp-content/uploads/2016/07/coastal-wash-web.jpg?fit=1024%2C682"
     }
 });
